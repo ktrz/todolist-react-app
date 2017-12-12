85b24cae15660764cb3e8a4ebc72eea9a5df1e54
Chris Trześniewski <krzysztof.trzesniewski@scalac.io>
2017-12-12 21:32:51 +0100

    installed react-bootstrap

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
+import 'bootstrap/dist/css/bootstrap-theme.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
 import React from 'react';
+import { Button, Jumbotron } from 'react-bootstrap'
 import ReactDOM from 'react-dom';
 
 const App = () => (
-  <h1>My first component</h1>
+  <Jumbotron>
+    <h1>Hello, group!</h1>
+    <p>We're going to create a simple todo list web application. We will use react-bootstrap components to make our app pretty</p>
+    <p>
+      <Button bsStyle="primary" onClick={() => alert('Let\'s start with ...')}>
+        Let's get started
+      </Button>
+    </p>
+  </Jumbotron>
 )
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
