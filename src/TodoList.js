41976d475e3d77faf84ee6e15df2a2a0e40af916
ktrz <k.trzesniewski@gmail.com>
2017-12-13 00:05:54 +0100

    Introduced stateful component

--- src/TodoList.js
+++ src/TodoList.js
@@ -4,16 +4,34 @@ import { TodoInput } from './TodoInput'
 import { TodoItem } from './TodoItem'
 import './TodoList.css'
 
-export const TodoList = () => (
-  <div className="todo-list">
-    <TodoInput value={'Test value'} onChange={(e) => console.log(e.target.value)}/>
-    <ListGroup>
-      <TodoItem itemName={'install NodeJS'} itemDescription={'Task description'}></TodoItem>
-      <TodoItem itemName={'install create-react-app'} itemDescription={'Task description'}></TodoItem>
-      <TodoItem itemName={'create new app'} itemDescription={'Task description'}></TodoItem>
-      <TodoItem itemName={'npm install'} itemDescription={'Task description'}></TodoItem>
-      <TodoItem itemName={'npm start'} itemDescription={'Task description'}></TodoItem>
-      <TodoItem itemName={'enjoy the running app'} itemDescription={'Task description'}></TodoItem>
-    </ListGroup>
-  </div>
-)
+export class TodoList extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      newItemValue: '',
+    }
+  }
+
+  render() {
+    return (
+      <div className="todo-list">
+        <TodoInput value={this.state.newItemValue} onChange={(e) => this.handleNewItemValue(e.target.value)}/>
+        <ListGroup>
+          <TodoItem itemName={'install NodeJS'} itemDescription={'Task description'}></TodoItem>
+          <TodoItem itemName={'install create-react-app'} itemDescription={'Task description'}></TodoItem>
+          <TodoItem itemName={'create new app'} itemDescription={'Task description'}></TodoItem>
+          <TodoItem itemName={'npm install'} itemDescription={'Task description'}></TodoItem>
+          <TodoItem itemName={'npm start'} itemDescription={'Task description'}></TodoItem>
+          <TodoItem itemName={'enjoy the running app'} itemDescription={'Task description'}></TodoItem>
+        </ListGroup>
+      </div>
+    )
+  }
+
+  handleNewItemValue(v) {
+    this.setState({
+      newItemValue: v,
+    })
+  }
+}
