e29461f217850c1971e6d21542b31d928b0a2a36
Chris Trześniewski <krzysztof.trzesniewski@scalac.io>
2017-12-12 23:07:27 +0100

    Added simple list of tasks todo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { Button, Jumbotron, Tab, Tabs } from 'react-bootstrap'
 import { TabbedExamples } from './examples/TabbedExamples'
+import { TodoList } from './TodoList'
 
-export const App = () => (
+export const App = () => [
   <Jumbotron>
     <h1>Hello, group!</h1>
     <p>We're going to create a simple todo list web application. We will use react-bootstrap components to make our app pretty</p>
@@ -11,13 +12,13 @@ export const App = () => (
         Let's get started
       </Button>
     </p>
-    <Tabs defaultActiveKey={1} id="main-tabs">
-      <Tab eventKey={1} title="Todo list app">
-        It's time to create our todo list app
-      </Tab>
-      <Tab eventKey={2} title="Examples">
-        <TabbedExamples/>
-      </Tab>
-    </Tabs>
-  </Jumbotron>
-)
+  </Jumbotron>,
+  <Tabs defaultActiveKey={1} id="main-tabs">
+    <Tab eventKey={1} title="Todo list app">
+      <TodoList/>
+    </Tab>
+    <Tab eventKey={2} title="Examples">
+      <TabbedExamples/>
+    </Tab>
+  </Tabs>,
+]
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,10 +1,16 @@
-const TodoList = () => (
-  <ListGroup>
-    <ListGroupItem>install NodeJS</ListGroupItem>
-    <ListGroupItem>install create-react-app</ListGroupItem>
-    <ListGroupItem>create new app</ListGroupItem>
-    <ListGroupItem>npm install</ListGroupItem>
-    <ListGroupItem>npm start</ListGroupItem>
-    <ListGroupItem>enjoy the running app</ListGroupItem>
-  </ListGroup>
+import React from 'react'
+import { ListGroup, ListGroupItem } from 'react-bootstrap'
+import './TodoList.css'
+
+export const TodoList = () => (
+  <div className="todo-list">
+    <ListGroup>
+      <ListGroupItem header={'install NodeJS'} active>Task description</ListGroupItem>
+      <ListGroupItem header={'install create-react-app'} bsStyle="danger">Task description (important task)</ListGroupItem>
+      <ListGroupItem header={'create new app'}>Task description</ListGroupItem>
+      <ListGroupItem header={'npm install'}>Task description</ListGroupItem>
+      <ListGroupItem header={'npm start'}>Task description</ListGroupItem>
+      <ListGroupItem header={'enjoy the running app'}>Task description</ListGroupItem>
+    </ListGroup>
+  </div>
 )
